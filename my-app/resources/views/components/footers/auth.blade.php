70d623d08da2648ca903641554970e1b72701a34
MariamW20 <164553072+MariamW20@users.noreply.github.com>
2024-07-03 12:46:47 +0300

    made some change

--- my-app/resources/views/components/footers/auth.blade.php
+++ my-app/resources/views/components/footers/auth.blade.php
@@ -8,9 +8,7 @@
 
                     </script>,
                     made with <i class="fa fa-heart"></i> by
-                    <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a> &
-                    <a href="https://www.updivision.com" class="font-weight-bold" target="_blank"> UPDIVISION</a>
-                    for a better web.
+                    G-32.
                 </div>
             </div>
             <div class="col-lg-6">

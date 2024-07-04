2813890bb5ab5ff41c9cc3d4724d5c9ac674e743
Mariam Wambui <mariamw107@gmail.com>
2024-07-04 16:15:12 +0300

    commit

--- the-laravel/resources/views/components/footers/guest.blade.php
+++ the-laravel/resources/views/components/footers/guest.blade.php
@@ -8,9 +8,7 @@
 
                     </script>,
                     made with <i class="fa fa-heart" aria-hidden="true"></i> by
-                    <a href="https://www.creative-tim.com" class="font-weight-bold text-white" target="_blank">Creative
-                        Tim</a> & <a href="https://www.updivision.com" class="font-weight-bold text-white" target="_blank">UPDIVISION</a>
-                    for a better web.
+                    G-32.
                 </div>
             </div>
             <div class="col-12 col-md-6">

b37a6101ae61198f4f97d730e4fcb0f07d082251
Mariam Wambui <mariamw107@gmail.com>
2024-07-05 20:10:36 +0300

    commit

--- the-laravel/resources/views/welcome.blade.php
+++ the-laravel/resources/views/welcome.blade.php
@@ -7,10 +7,10 @@
         </div>
     </div>
     <div class="page-header justify-content-center min-vh-100"
-        style="background-image: url('https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80');">
+        style="background-image: url('myMaths.jpg');">
         <span class="mask bg-gradient-dark opacity-6"></span>
         <div class="container">
-            <h1 class="text-light text-center">Welcome to the Math Quest Challenge.</h1>
+            <h1 class="text-light text-center">Welcome to the Math Quest Challenge System.</h1>
         </div>
     </div>
         <x-footers.guest></x-footers.guest>

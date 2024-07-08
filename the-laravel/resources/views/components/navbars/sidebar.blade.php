61b27cbe83df9485ff7cbdbe77b5432430098938
Mariam Wambui <mariamw107@gmail.com>
2024-07-08 13:28:31 +0300

    added logo and some changes

--- the-laravel/resources/views/components/navbars/sidebar.blade.php
+++ the-laravel/resources/views/components/navbars/sidebar.blade.php
@@ -7,7 +7,7 @@
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0 d-flex text-wrap align-items-center" href=" {{ route('dashboard') }} ">
-            <img src="{{ asset('assets') }}/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
+            <img src="{{ asset('assets') }}/img/mathquest.jpg" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-2 font-weight-bold text-white">The Math Quest Challenge System</span>
         </a>
     </div>
@@ -15,7 +15,7 @@
     <div class="collapse navbar-collapse  w-auto  max-height-vh-100" id="sidenav-collapse-main">
         <ul class="navbar-nav">
             <li class="nav-item mt-3">
-                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Laravel examples</h6>
+                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">ITEMS </h6>
             </li>
             <li class="nav-item">
                 <a class="nav-link text-white {{ $activePage == 'user-profile' ? 'active bg-gradient-primary' : '' }} "
@@ -121,18 +121,5 @@
                 </a>
             </li>
         </ul>
-    </div>
-    <div class="sidenav-footer position-absolute w-100 bottom-0 ">
-        <div class="mx-3">
-            <a class="btn bg-gradient-primary w-100" href="https://www.creative-tim.com/product/material-dashboard-laravel" target="_blank">Free Download</a>
-        </div>
-        <div class="mx-3">
-            <a class="btn bg-gradient-primary w-100" href="../../documentation/getting-started/installation.html" target="_blank">View documentation</a>
-        </div>
-        <div class="mx-3">
-            <a class="btn bg-gradient-primary w-100"
-                href="https://www.creative-tim.com/product/material-dashboard-pro-laravel" target="_blank" type="button">Upgrade
-                to pro</a>
-        </div>
     </div>
 </aside>
--- the-laravel/resources/views/pages/static-sign-in.blade.php
+++ the-laravel/resources/views/pages/static-sign-in.blade.php
@@ -17,7 +17,7 @@
                     <div class="col-lg-4 col-md-8 col-12 mx-auto">
                         <div class="card z-index-0 fadeIn3 fadeInBottom">
                             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                                <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
+                                <div class="bg-gradient-dark shadow-dark border-radius-lg py-3 pe-1">
                                     <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Sign in</h4>
                                     <div class="row mt-3">
                                         <div class="col-2 text-center ms-auto">
@@ -54,13 +54,13 @@
                                             me</label>
                                     </div>
                                     <div class="text-center">
-                                        <button type="submit" class="btn bg-gradient-primary w-100 my-4 mb-2">Sign
+                                        <button type="submit" class="btn bg-gradient-dark w-100 my-4 mb-2">Sign
                                             in</button>
                                     </div>
                                     <p class="mt-4 text-sm text-center">
                                         Don't have an account?
                                         <a href="{{ route('static-sign-up') }}"
-                                            class="text-primary text-gradient font-weight-bold">Sign up</a>
+                                            class="text-dark text-gradient font-weight-bold">Sign up</a>
                                     </p>
                                 </form>
                             </div>

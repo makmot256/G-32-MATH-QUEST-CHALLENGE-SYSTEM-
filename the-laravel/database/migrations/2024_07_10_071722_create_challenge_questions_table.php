ec0c931cf48c5f978dde4dd6651175ba54d683eb
Mariam Wambui <mariamw107@gmail.com>
2024-07-10 15:13:43 +0300

    added changes

---
+++ the-laravel/database/migrations/2024_07_10_071722_create_challenge_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('challenge_questions', function (Blueprint $table) {
+            $table->integer('id')->primary()->autoIncrement();
+            $table->integer('challenge_id');
+            $table->foreign('challenge_id')->references('id')->on('challenges');
+            $table->integer('question_id');
+            $table->foreign('question_id')->references('id')->on('questions');
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('challenge_questions');
+    }
+};
---
+++ the-laravel/database/migrations/2024_07_10_071723_create_challenges_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('challenges', function (Blueprint $table) {
+            $table->integer('id')->primary()->autoIncrement();
+            $table->string('name', 255);
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('duration');
+            $table->integer('num_questions');
+            $table->string('description', 255);
+            $table->timestamp('created_at')->nullable()->useCurrent();
+            $table->timestamp('updated_at')->nullable()->useCurrent();
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('challenges');
+    }
+};

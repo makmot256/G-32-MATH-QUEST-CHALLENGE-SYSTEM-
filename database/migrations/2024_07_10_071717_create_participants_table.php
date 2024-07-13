7cd00f1ddeb7b179d107da64bfda9940f034bd49
Bridget <bridgetbataringaya7@gmail.com>
2024-07-13 11:58:20 +0300

    only changed the last munber to 717,and removed the increment

---
+++ database/migrations/2024_07_10_071717_create_participants_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('participants', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->string('username', 255)->unique();
+            $table->string('firstname', 255);
+            $table->string('lastname', 255);
+            $table->string('school_registration_number', 255)->nullable();
+            $table->foreign('school_registration_number')->references('school_registration_number')->on('schools');
+            $table->string('email', 255);
+            $table->date('date_of_birth');
+            $table->string('status', 255)->default('confirmed');
+            $table->string('password', 255);
+            $table->integer('image')->nullable();
+            $table->timestamp('created_at')->nullable()->useCurrent();
+            $table->timestamp('updated_at')->nullable()->useCurrent();
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('participants');
+    }
+};

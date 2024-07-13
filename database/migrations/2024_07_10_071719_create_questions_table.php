a0d6816e72d46e2bf86f66e6c81ffb3fe1990a85
Bridget <bridgetbataringaya7@gmail.com>
2024-07-13 12:03:11 +0300

    didnt change anything in the code except removing the increment,and also changed the last number to 719

---
+++ database/migrations/2024_07_10_071719_create_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('questions', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->text('question_text');
+            $table->text('answer');
+            $table->integer('marks');
+            $table->timestamp('created_at')->nullable()->useCurrent();
+            $table->timestamp('updated_at')->nullable()->useCurrent();
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};

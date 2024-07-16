14c8e4fe1064db21624d0b6d9c1e13ccc78b39d4
Mariam Wambui <mariamw107@gmail.com>
2024-07-16 11:50:59 +0300

    commit

--- the-laravel/database/migrations/2024_07_10_071719_create_questions_table.php
+++ the-laravel/database/migrations/2024_07_10_071719_create_questions_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
 
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
-            $table->text('question_text');
-            $table->text('answer');
+            $table->text('question_text')->nullable();
+            $table->text('answer')->nullable();
             $table->integer('marks');
             $table->timestamp('created_at')->nullable()->useCurrent();
             $table->timestamp('updated_at')->nullable()->useCurrent();

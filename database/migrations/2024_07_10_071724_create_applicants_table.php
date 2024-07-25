2850455292fadedbea82db4bbc4441c10c633b50
MAKMOT JOHNSON <makmotjohnson7@gmail.com>
2024-07-25 11:06:42 +0300

    Add files via upload

---
+++ database/migrations/2024_07_10_071724_create_applicants_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('applicants', function (Blueprint $table) {
+            $table->id();
+            $table->string('username', 255)->unique();
+            $table->string('firstname', 255);
+            $table->string('lastname', 255);
+            $table->string('school_registration_number', 255)->nullable();
+            $table->string('email', 255);
+            $table->date('date_of_birth');
+            $table->string('password', 255);
+            $table->integer('image')->nullable();
+            $table->timestamp('created_at')->nullable()->useCurrent();
+            $table->timestamp('updated_at')->nullable()->useCurrent();
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('applicants');
+    }
+};
